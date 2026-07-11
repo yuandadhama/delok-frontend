ae07a2f3550e4d54cb06bcd8760847d8ab045772
yuandadhama <yuandadhamap@gmail.com>
2026-07-11 15:39:51 +0700

    feat(dashboard): fetch data organizations and show them added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,16 +1,38 @@
+// /app/dashboard
+
 "use client";
 
 import Button from "@/src/component/ui/Button";
 import Input from "@/src/component/ui/Input";
 import { authClient } from "@/src/lib/auth-client";
 import { useRouter } from "next/navigation";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+type Organization = {
+  id: string;
+  name: string;
+};
 
 export default function Dashboard() {
   const { data: session, isPending } = authClient.useSession();
   const router = useRouter();
 
   const [organizationName, setOrganizationName] = useState("");
+  const [organizations, setOrganizations] = useState<Organization[]>([]);
+
+  const fetchOrganization = async () => {
+    const response = await fetch("http://localhost:8000/api/organization", {
+      credentials: "include",
+    });
+    const data = await response.json();
+    setOrganizations(data.data);
+    console.log("fetched organization data: " + JSON.stringify(data));
+  };
+
+  useEffect(() => {
+    if (!session?.user?.id) return;
+    fetchOrganization();
+  }, [session]);
 
   if (isPending) {
     return <div>Loading...</div>;
@@ -23,6 +45,7 @@ export default function Dashboard() {
   const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
     try {
       e.preventDefault();
+
       const response = await fetch(
         "http://localhost:8000/api/organization/create",
         {
@@ -34,40 +57,61 @@ export default function Dashboard() {
           }),
         },
       );
-      const data = response.json();
-      console.log("response create org: " + data);
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setOrganizationName("");
+        await fetchOrganization();
+      }
+      console.log("response create org: " + JSON.stringify(data));
     } catch (e) {
       console.error(e);
     }
   };
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome {session.user.name}</p>
-      <form onSubmit={handleSubmit}>
-        <Input
-          label="organization name"
-          name="name"
-          onChange={(e) => setOrganizationName(e.target.value)}
-          value={organizationName}
-        />
-        <Button className="bg-green-600">create</Button>
-      </form>
-      <button
-        className="bg-red-500 text-white p-2 rounded hover:opacity-80 cursor-pointer"
-        onClick={() => {
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => {
-                router.push("/");
-              },
-            },
-          });
-        }}
-      >
-        Log out
-      </button>
+    <div className="flex justify-center  w-full h-screen">
+      <div className="w-full container flex flex-col gap-4 p-8">
+        <header className="flex justify-between">
+          <div>
+            <h1>Dashboard</h1>
+            <p>Welcome {session.user.name}</p>
+          </div>
+
+          <button
+            className="bg-red-500 text-white p-2 rounded hover:opacity-80 cursor-pointer"
+            onClick={() => {
+              authClient.signOut({
+                fetchOptions: {
+                  onSuccess: () => {
+                    router.push("/");
+                  },
+                },
+              });
+            }}
+          >
+            Log out
+          </button>
+        </header>
+        <form onSubmit={handleSubmit}>
+          <Input
+            label="Create New Organization"
+            name="name"
+            placeholder="organization name"
+            onChange={(e) => setOrganizationName(e.target.value)}
+            value={organizationName}
+          />
+          <Button className="bg-green-600 mt-4">create</Button>
+        </form>
+
+        <h2 className="text-2xl font-bold">Your Organization</h2>
+        <ul>
+          {organizations.map((organization) => (
+            <li key={organization.id}>{organization.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
