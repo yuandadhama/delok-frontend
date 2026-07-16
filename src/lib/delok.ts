be3016500e10d4d61d21c3e349de0131c94803d9
yuandadhama <yuandadhamap@gmail.com>
2026-07-16 20:28:34 +0700

    refactor: change delok api key with the correct one

--- src/lib/delok.ts
+++ src/lib/delok.ts
@@ -2,6 +2,6 @@ import { Delok } from "delok";
 
 export const delok = new Delok({
   apiKey:
-    "dlok_79c6f2777f72141f28410b908c5ce02659ede38dc7b9097527582a49d1460cb6",
+    "dlok_038eb0209e703cf34e0b2e197808b0627f283a12d072a00a7a2bfef9f2061e69",
   environment: "development",
 });
